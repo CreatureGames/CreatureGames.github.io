2df31539aa2ba45d24547f67ff6035bfae0a6f25
rschubkegel <rschubkegel@users.noreply.github.com>
2022-09-04 22:04:51 +0000

    deploy: acd0df6631df39104736600602df357b00557642

--- _nuxt/static/1662329030/About/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662329030",layout:"default",error:a,serverRendered:true,routePath:"\u002FAbout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"11993743"}},_img:{"/_ipx/s_64x64/creature-icon.png":"\u002F_nuxt\u002Fimage\u002F1c9ca4.png"}}}(null));
---
+++ _nuxt/static/1662329090/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662329090",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5802ed15"}},_img:{"/_ipx/s_64x64/creature-icon.png":"\u002F_nuxt\u002Fimage\u002F1c9ca4.png"}}}(null,"\u002F"));
