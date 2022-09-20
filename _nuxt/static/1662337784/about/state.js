a69740d435cf75673b6e1d9b76a3665e4d744360
rschubkegel <rschubkegel@users.noreply.github.com>
2022-09-20 05:47:56 +0000

    deploy: 715d0ce17e1dd1a8bb76b03ce2169cebaf071c00

--- _nuxt/static/1662337784/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662337784",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ecb1d819"}},_img:{"/_ipx/s_64x64/creature.gif":"\u002F_nuxt\u002Fimage\u002Fa265c1.gif"}}}(null));
